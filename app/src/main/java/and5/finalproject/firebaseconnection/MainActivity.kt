28d7f6dc00deaf6c5947644552103ca04fb2b0f3
zeph1re <arlinnog1122@gmail.com>
2023-03-16 16:55:26 +0700

    add firebase connection

--- app/src/main/java/and5/finalproject/firebaseconnection/MainActivity.kt
+++ app/src/main/java/and5/finalproject/firebaseconnection/MainActivity.kt
@@ -1,11 +1,36 @@
 package and5.finalproject.firebaseconnection
 
+import android.content.ContentValues.TAG
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+
 
 class MainActivity : AppCompatActivity() {
+
+    val database = Firebase.firestore
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Create a new user with a first and last name
+        val user = hashMapOf(
+            "first" to "Ada",
+            "last" to "Lovelace",
+            "born" to 1815
+        )
+
+// Add a new document with a generated ID
+        database.collection("users")
+            .add(user)
+            .addOnSuccessListener { documentReference ->
+                Log.d(TAG, "DocumentSnapshot added with ID: ${documentReference.id}")
+            }
+            .addOnFailureListener { e ->
+                Log.w(TAG, "Error adding document", e)
+            }
     }
 }
